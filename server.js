9ba8ae2edb371487a2880655d95d79d05aff5343
harinadh <saiharinadhpv@gmail.com>
2026-04-24 14:36:32 +0000

    updated server.js file1

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || "dummy";
 process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || "dummy";
 
 dynamodbAdmin.start({
-  port: 8001,
+  port: 8080,
 });

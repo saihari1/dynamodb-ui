1c5de27a32c8259b156695233bb720a3bdcf3df6
harinadh <saiharinadhpv@gmail.com>
2026-04-24 14:40:46 +0000

    updated server.js file in endpoint url

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const dynamodbAdmin = require("dynamodb-admin");
 
-process.env.DYNAMODB_ENDPOINT = process.env.DYNAMODB_ENDPOINT;
+process.env.DYNAMO_ENDPOINT = process.env.DYNAMO_ENDPOINT;
 process.env.AWS_REGION = process.env.AWS_REGION || "us-east-1";
 process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || "dummy";
 process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || "dummy";
